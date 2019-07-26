c1e1563995cb91f02e5f54726a63c29c538c60f5
1170300802 <42999043+1170300802@users.noreply.github.com>
2019-07-26 23:04:48 +0800

    Update quicksort.py

--- General_Sorting/quicksort.py
+++ General_Sorting/quicksort.py
@@ -1,54 +1,29 @@
-#!/usr/bin/python2
-# -*- coding: utf-8 -*-
-"""
-Quicksort... You know, right? :)
-"""
-__author__ = "Maurice Tollmien"
-__maintainer__ = "Maurice Tollmien"
-__email__ = "maurice.tollmien@gmail.com"
-
-from random import randint
 import random
-import time
 
-def split(leftI, rightI):
-    pivot = rightI
-    i = leftI
-    j = rightI
-    while i < j:
-        while l[i] <= l[pivot] and i < rightI:
-            i = i+1
-        while l[j] >= l[pivot] and j >= leftI:
-            j = j-1
-        if i < j:
-            tmp = l[j]
-            l[j] = l[i]
-            l[i] = tmp
-    if l[i] > l[pivot]:
-        tmp = l[pivot]
-        l[pivot] = l[i]
-        l[i] = tmp
-    return i
-
-def quickSort (leftI, rightI):
-    if leftI < rightI:
-        splitter = split(leftI, rightI)
-        quickSort(leftI, splitter-1)
-        quickSort(splitter+1, rightI)
-
-
-# Some small tests
-if __name__ == '__main__':
-    size = 1000000
-    l = range(size)
-    #random.shuffle(l)
-
-    #l.reverse()
-
-    t0 = time.time()
-
-    quickSort (0, size-1)
-
-    print time.time() - t0
-
-    #print l
+def quicksort(arr, left, right):
+    #only if left < right , go on
+    if left >=right:
+        return 
+    random_index = random.randint(left, right)
+    #swap privot and first
+    arr[left], arr[random_index] = arr[random_index], arr[left]
+    pivot = arr[left]
+    lt = left # arr[left+1...lt] < v
+    gt = right + 1 # arr[gt...right] > v
+    i = left + 1 # arr[lt+1...i] == v
+    #when i = gt, stop
+    while i < gt:
+        if arr[i] < pivot:
+            arr[i], arr[lt+1] = arr[lt+1], arr[i]
+            lt += 1
+            i += 1
+        elif arr[i] > pivot:
+            arr[i], arr[gt-1] = arr[gt-1], arr[i]
+            gt -= 1
+        else:
+            i += 1
+    #swap first(privot) to v
+    arr[left], arr[lt] = arr[lt], arr[left]
+    #iterator
+    quicksort(arr, left, lt-1)
+    quicksort(arr, gt, right)
